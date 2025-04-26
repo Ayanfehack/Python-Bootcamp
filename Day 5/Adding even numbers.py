28b203ecc056a6b7b342343c3f314cff34245fae
Oyinloluwa Odekanyin <ayanfeodekanyin3103@gmail.com>
2025-04-26 10:08:32 +0100

    Add files via upload

---
+++ Day 5/Adding even numbers.py
@@ -0,0 +1,7 @@
+number = 0
+for even in range(1, 101):
+    if even % 2 == 0:
+        number += even
+
+print(number)
+
---
+++ Day 5/FizzBuzz.py
@@ -0,0 +1,18 @@
+
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
+
+# print(number)
+x = 1.0
+y = 2.0
+w = 3.0
+
+z=2.0(x(y+3.0)+w)
+print(z)
