ec408ad1851c13f1bb565c6dd6be92228b50a42a
Oyinloluwa Odekanyin <ayanfeodekanyin3103@gmail.com>
2025-04-26 09:55:17 +0100

    Add files via upload

---
+++ Day 1/BANDNAME CREATOR.py
@@ -0,0 +1,4 @@
+print("Good Day, Welcome to the Band Name Creator.")
+city = input("What city did you grow up in?\n")
+pet = input("What's the name of your pet?\n")
+print("The name of your Band is " + (city + pet) + "\n")
